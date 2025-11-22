34fd4f8622eb3095fd0d205ab0e778a09c510197
Rahul Singh <rahul660singh@gmail.com>
2025-11-22 11:57:40 +0530

    Authentication completed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "IntelliTrack" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Welcome to IntelliTrack</h1>
+          <p>
+            Your smart assistant for tracking job applications and receiving personalized
+            feedback.
+          </p>
+        </div>
+      
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+    ))}
+
+      </div>
+    )} 
+
+</section>
+
+    </main>
+  );
 }
